23f120d85758494c429193a87764e8b41fc2f857
lif314 <2216155077@qq.com>
2022-06-07 16:40:33 +0800

    修复Bug

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -2,27 +2,12 @@
   <div class="mod-home">
     <h3>项目介绍</h3>
     <ul>
-      <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li>
-      <li>前后端分离，通过token进行数据交互，可独立部署</li>
-      <li>主题定制，通过scss变量统一一站式定制</li>
-      <li>动态菜单，通过菜单管理统一管理访问路由</li>
-      <li>数据切换，通过mock配置对接口数据／mock模拟数据进行切换</li>
-      <li>发布时，可动态配置CDN静态资源／切换新旧版本</li>
-      <li>演示地址：<a href="http://demo.open.renren.io/renren-fast" target="_blank">http://demo.open.renren.io/renren-fast</a> (账号密码：admin/admin)</li>
+      <li>本组选题为智能家居软件生态系统，是上海市大学生创新项目的扩展(2022年国创项目)，主要组成部分为线上商城系统、后台管理系统以及智能设备管理系统。</li>
+      <li>在功能上，涵盖基本的用户认证、商品检索、订单服务、支付功能等，也包括高并发场景下的商品秒杀服务、权限分级的商城后台管理服务、基于推荐算法的商品推荐服务、基于人脸识别的免密登录服务等；</li>
+      <li>在业务内容上，除了基本的商城平台业务，我们还会开设专营智能设备的官方旗舰店，并整合智能设备管理系统为客户提供后续服务。</li>
     </ul>
-    <h3>获取帮助</h3>
-    <ul>
-      <li>官方社区：<a href="https://www.renren.io/community" target="_blank">https://www.renren.io/community</a></li>
-      <li>前端Git地址：<a href="https://github.com/renrenio/renren-fast-vue" target="_blank">https://github.com/renrenio/renren-fast-vue</a></li>
-      <li>后台Git地址：<a href="https://gitee.com/renrenio/renren-fast" target="_blank">https://gitee.com/renrenio/renren-fast</a></li>
-      <li>代码生成器：<a href="https://gitee.com/renrenio/renren-generator" target="_blank">https://gitee.com/renrenio/renren-generator</a></li>
-      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
-    </ul>
-    <h3>官方QQ群</h3>
-    <ul>
-      <li>高级群：324780204(大牛云集，跟大牛学习新技能)</li>
-      <li>普通群：145799952(学习交流，互相解答各种疑问)</li>
-    </ul>
+    <h3>项目蓝图</h3>
+    <img style="width: 75%; height: 75%" src="../../assets/img/xiaoji.png" alt="架构">
   </div>
 </template>
 
